222ed3793aab7487eab1487f28901ac326faf295
Krzysztof Czereczon <kczereczon@gmail.com>
2023-08-21 13:13:42 +0200

    added ci

--- src/CommandHandler/SignIntoNewsletterHandler.php
+++ src/CommandHandler/SignIntoNewsletterHandler.php
@@ -2,11 +2,17 @@
 
 namespace App\CommandHandler;
 
+use App\Command\SignIntoNewsletterCommand;
 use App\Common\CQRS\CommandHandler;
 
 class SignIntoNewsletterHandler implements CommandHandler
 {
     public function __construct()
     {
+    }
+
+    public function __invoke(SignIntoNewsletterCommand $command)
+    {
+        // TODO: Implement __invoke() method.
     }
 }
---
+++ src/Repository/EmailAddressRepository.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\EmailAddress;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<EmailAddress>
+ *
+ * @method EmailAddress|null find($id, $lockMode = null, $lockVersion = null)
+ * @method EmailAddress|null findOneBy(array $criteria, array $orderBy = null)
+ * @method EmailAddress[]    findAll()
+ * @method EmailAddress[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EmailAddressRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, EmailAddress::class);
+    }
+
+//    /**
+//     * @return EmailAddress[] Returns an array of EmailAddress objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?EmailAddress
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+
+    public function findActiveEmailAddresses(): array
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.disabled = :val')
+            ->setParameter('val', false)
+            ->getQuery()
+            ->getArrayResult()
+        ;
+    }
+}
